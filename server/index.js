a5cf2fd78e008dd1be179e7d12d4db9259bc2056
Ashwani Kharwar <ashwanikumarkharwar83@gmail.com>
2025-12-17 00:19:11 +0530

    add new url

--- server/index.js
+++ server/index.js
@@ -27,7 +27,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"https://study-notion-by-ashwanikharwar.vercel.app",
+		origin:["https://study-notion-by-ashwanikharwar.vercel.app",
+			"http://localhost:3000",
+			"https://studynotion.ashwanikharwar.dev/"
+		],
 		credentials:true,
 	})
 )
